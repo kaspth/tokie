d91ce85d6f65b78e5f179a3d100c6d993849cd62
Kasper Timm Hansen <kaspth@gmail.com>
2014-10-14 19:26:24 +0200

    Scope concerns to Tokie module.

--- lib/tokie/concerns/digestable.rb
+++ lib/tokie/concerns/digestable.rb
@@ -1,11 +1,11 @@
-require 'active_support/concern'
+require 'active_support/core_ext/module/concerning'
 
-module Digestable
-  extend ActiveSupport::Concern
-
-  private
-    def generate_digest(data)
-      require 'openssl' unless defined?(OpenSSL)
-      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.const_get(@digest).new, @secret, data)
-    end
+module Tokie
+  concern :Digestable do
+    private
+      def generate_digest(data)
+        require 'openssl' unless defined?(OpenSSL)
+        OpenSSL::HMAC.hexdigest(OpenSSL::Digest.const_get(@digest).new, @secret, data)
+      end
+  end
 end
