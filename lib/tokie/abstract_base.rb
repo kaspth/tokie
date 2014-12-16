fcd71ab9624c186d3eab7e731a66179e69e240ee
Kasper Timm Hansen <kaspth@gmail.com>
2014-12-16 15:22:21 +0100

    Inline `Digestable` and `SecureComparable` concerns.

--- lib/tokie/abstract_base.rb
+++ lib/tokie/abstract_base.rb
@@ -1,12 +1,8 @@
 require 'active_support/core_ext/object/blank'
-require 'tokie/concerns/digestable'
-require 'tokie/concerns/secure_comparable'
 require 'tokie/errors'
 
 module Tokie
   class AbstractBase
-    include Digestable, SecureComparable
-
     def initialize(claims, secret: Tokie.secret, digest: 'SHA1', serializer: Tokie.serializer)
       @claims = claims
       @secret = secret
@@ -29,6 +25,23 @@ module Tokie
 
       def untampered?(digest, data)
         secure_compare digest, generate_digest(data)
+      end
+
+      def generate_digest(data)
+        require 'openssl' unless defined?(OpenSSL)
+        OpenSSL::HMAC.hexdigest(OpenSSL::Digest.const_get(@digest).new, @secret, data)
+      end
+
+      # FIXME: Ditch in favor of ActiveSupport::SecurityUtils once 4.2 ships.
+      # constant-time comparison algorithm to prevent timing attacks
+      def secure_compare(a, b)
+        return false unless a.bytesize == b.bytesize
+
+        l = a.unpack "C#{a.bytesize}"
+
+        res = 0
+        b.each_byte { |byte| res |= byte ^ l.shift }
+        res == 0
       end
   end
 end
