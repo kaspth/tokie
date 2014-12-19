567953dca287810cc4e789fca747976c060e5b7e
Kasper Timm Hansen <kaspth@gmail.com>
2014-12-19 18:32:57 +0100

    Extract Token to own file and autoload it.

--- lib/tokie.rb
+++ lib/tokie.rb
@@ -21,49 +21,5 @@ module Tokie
     end
   end
 
-  class Token
-    def initialize(payload_or_token, claim_options = {})
-      @payload = extract_payload(payload_or_token)
-      @claim_options = claim_options
-    end
-
-    def payload
-      claims.payload
-    end
-
-    def to_s
-      @encoded_data
-    end
-
-    def sign(options = {})
-      @encoded_data = Signer.new(claims, options).sign
-    end
-
-    def verify(options = {})
-      parse_claims Signer.new(@payload, options).verify
-      self
-    end
-
-    def encrypt(options = {})
-      @encoded_data = Encryptor.new(claims, options).encrypt
-    end
-
-    def decrypt(options = {})
-      parse_claims Encryptor.new(@payload, options).decrypt
-      self
-    end
-
-    private
-      def extract_payload(token)
-        token.respond_to?(:payload) ? token.payload : token
-      end
-
-      def parse_claims(data)
-        @claims = Claims.parse(data, @claim_options)
-      end
-
-      def claims
-        @claims ||= Claims.new(@payload, @claim_options)
-      end
-  end
+  autoload :Token, 'tokie/token'
 end
